25bab01cde9d71d4298afb432c48678efe8dbb84
Tom Magin <magintc@mac.com>
2021-02-14 14:31:13 +1100

    #fixing last bit of logic to render score at end

--- main.js
+++ main.js
@@ -1,188 +1,187 @@
 let nScore = 0;
-function check() {
+let pScore = 0;
+
+function question1() {
   let question1 = document.getElementsByName("q1");
-  let question2 = document.getElementsByName("q2");
-  let question3 = document.getElementsByName("q3");
-  let question4 = document.getElementsByName("q4");
-  let question5 = document.getElementsByName("q5");
-  let question6 = document.getElementsByName("q6");
-  let question7 = document.getElementsByName("q7");
-  let question8 = document.getElementsByName("q8");
-  let question9 = document.getElementsByName("q9");
-  let question10 = document.getElementsByName("q10");
-
-  //   console.log(question1);
-
   for (let i = 0; i < question1.length; i++) {
     if (question1[i].checked) {
       if (question1[i].value == "c") {
-        //  question1[i].value;
-        console.log("checked correct answer", question1[i].value);
-        //   if (q1[i].value === "C") {
-        nScore += 10;
-        return progressBarUpdate(nScore);
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
       } else {
-        console.log("not checked");
-        return;
-        // }
-      }
-    }
-  }
-  for (let i = 0; i < question2.length; i++) {
-    if (question2[i].checked) {
-      if (question2[i].value == "c") {
-        //  question1[i].value;
-        console.log("checked correct answer", question2[i].value);
-        //   if (q1[i].value === "C") {
-        nScore += 10;
-        return progressBarUpdate(nScore);
-      } else {
-        console.log("not checked");
-        return;
-        // }
-      }
-    }
-  }
-  for (let i = 0; i < question3.length; i++) {
-    if (question3[i].checked) {
-      if (question3[i].value == "a") {
-        console.log("checked correct answer", question3[i].value);
-
-        nScore += 10;
-        return progressBarUpdate(nScore);
-      } else {
-        console.log("not checked");
-        return;
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
       }
     }
   }
 }
-
-check();
-// function fQuestion1() {
-//   let sCorrectAnswer = document.getElementById("q1c");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-// }
-
-// function fQuestion2() {
-//   let sCorrectAnswer = document.getElementById("q2c");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-// }
-
-// function fQuestion3() {
-//   let sCorrectAnswer = document.getElementById("q3a");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-//   document.getElementById("txtScore").value = nScore;
-// }
-
-// function fQuestion4() {
-//   let sCorrectAnswer = document.getElementById("q4b");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-// }
-// function fQuestion5() {
-//   let sCorrectAnswer = document.getElementById("q5d");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-// }
-// function fQuestion6() {
-//   let sCorrectAnswer = document.getElementById("q6a");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-// }
-// function fQuestion7() {
-//   let sCorrectAnswer = document.getElementById("q7b");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-// }
-// function fQuestion8() {
-//   let sCorrectAnswer = document.getElementById("q8c");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-// }
-// function fQuestion9() {
-//   let sCorrectAnswer = document.getElementById("q9d");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-// }
-// function fQuestion10() {
-//   let sCorrectAnswer = document.getElementById("q10a");
-
-//   if (sCorrectAnswer.checked === true) {
-//     nScore += 10;
-//     progressBarUpdate(nScore);
-//     total += 1;
-//     console.log(total);
-//   } else {
-//     nScore -= 0;
-//   }
-// }
+function question2() {
+  let question2 = document.getElementsByName("q2");
+  for (let i = 0; i < question2.length; i++) {
+    if (question2[i].checked) {
+      if (question2[i].value == "c") {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
+      } else {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
+      }
+    }
+  }
+}
+function question3() {
+  let question3 = document.getElementsByName("q3");
+  for (let i = 0; i < question3.length; i++) {
+    if (question3[i].checked) {
+      if (question3[i].value == "a") {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
+      } else {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
+      }
+    }
+  }
+}
+function question4() {
+  let question4 = document.getElementsByName("q4");
+  for (let i = 0; i < question4.length; i++) {
+    if (question4[i].checked) {
+      if (question4[i].value == "b") {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
+      } else {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
+      }
+    }
+  }
+}
+function question5() {
+  let question5 = document.getElementsByName("q5");
+  for (let i = 0; i < question5.length; i++) {
+    if (question5[i].checked) {
+      if (question5[i].value == "d") {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
+      } else {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
+      }
+    }
+  }
+}
+function question6() {
+  let question6 = document.getElementsByName("q6");
+  for (let i = 0; i < question6.length; i++) {
+    if (question6[i].checked) {
+      if (question6[i].value == "c") {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
+      } else {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
+      }
+    }
+  }
+}
+function question7() {
+  let question7 = document.getElementsByName("q7");
+  for (let i = 0; i < question7.length; i++) {
+    if (question7[i].checked) {
+      if (question7[i].value == "b") {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
+      } else {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
+      }
+    }
+  }
+}
+function question8() {
+  let question8 = document.getElementsByName("q8");
+  for (let i = 0; i < question8.length; i++) {
+    if (question8[i].checked) {
+      if (question8[i].value == "c") {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
+      } else {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
+      }
+    }
+  }
+}
+function question9() {
+  let question9 = document.getElementsByName("q9");
+  for (let i = 0; i < question9.length; i++) {
+    if (question9[i].checked) {
+      if (question9[i].value == "d") {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
+      } else {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
+      }
+    }
+  }
+}
+function question10() {
+  let question10 = document.getElementsByName("q10");
+  for (let i = 0; i < question10.length; i++) {
+    if (question10[i].checked) {
+      if (question10[i].value == "a") {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore += 10;
+        percentageScore(pScore);
+      } else {
+        nScore += 25;
+        progressBarUpdate(nScore);
+        pScore -= 0;
+        percentageScore(pScore);
+      }
+      percentageScore(pScore);
+    }
+  }
+}
 
 function progressBarUpdate(value) {
   value = Math.round(value);
@@ -190,4 +189,13 @@ function progressBarUpdate(value) {
     ".progress-fill"
   ).style.width = `${value}`);
   return progressBar;
+}
+
+function percentageScore(value) {
+  value = Math.round(value);
+  let percentScore = (document.querySelector(
+    ".progress-title"
+  ).innerHTML = `${value}%`);
+  percentScore.style.display = "block";
+  return percentScore;
 }
